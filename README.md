2aa349d4bea13d6b675bc0db53014a0dc5d47160
Kaleb Dufera <kdufera@gmail.com>
2019-03-07 22:58:20 -0500

    updated Readme.md

---
+++ README.md
@@ -0,0 +1,54 @@
+
+
+# PRODUCT_ORDER_ENTY_SYSTEM
+
+This application incorporates RESTful API's that can receive an order with a customer number and the product IDs and quantities of the items ordered. The application initially calls an established inventory API to get a list of product name and price. An order can be placed and retrieved via the API's specified below.
+
+
+# Instruction 
+ 
+* MongoDB is required for this project 
+    * run 'nongod' 
+
+* Clone the repo and run 'npm install'
+
+* run 'node .' or 'nodmon .'
+
+
+# Main API's
+
+Get order summery:
+* http://localhost:3000/order/v1/orderSummery/
+
+    
+Get Single Product:
+*  http://localhost:3000/product/v1/petProduct/8ed0e6f7
+
+    {
+        "name": "",
+        "price": ""
+    }
+
+
+POST a single Product:
+* http://localhost:3000/product/v1/petProduct
+
+    {
+        "id": "",
+        "name": "",
+        "price": 0
+    }
+
+POST a Single Order:
+* http://localhost:3000/order/v1/userOrder
+
+    {
+    "customerId": "string",
+    "items": [
+        {
+        "productId": "string",
+        "quantity": 0
+        }
+    ]
+    }
+
--- server.js
+++ server.js
@@ -15,34 +15,33 @@ app.use(express.json());
 /**
 * Route configuration for different endpoints
 */
- app.use('/product/v1', productRoter);  // user authorization route
- app.use('/order/v1', orderRoter);  // user authorization route
+ app.use('/product/v1', productRoter);  
+ app.use('/order/v1', orderRoter); 
 
 
 
- // fetch data from API,
-//  fetch(process.env.PRODUCTS_API).then((res) => {
-
-//      if(res.status == 200) {
-//          return res.json();
-//      } else {
-//          console.log("unable to fetch products");
-//      } 
-//  }).then((resProducts) => {
-//       resProducts.body.forEach(function(element) {
-//         fetch(process.env.FETCH_PRODUCTS_API, {
-//             method: 'post',
-//             body:    JSON.stringify(element),
-//             headers: { 'Content-Type': 'application/json' },
-//         }).then((res) => {
-//             if(res.status != 200) {
-//                 console.log("unable to save products");
-//             } else {
-//                 console.log("saved products to DB");
-//             }
-//         });
-//      });
-// });
+ //fetch data from API ( Initial load)
+ fetch(process.env.PRODUCTS_API).then((res) => {
+     if(res.status == 200) {
+         return res.json();
+     } else {
+         console.log("unable to fetch products");
+     } 
+ }).then((resProducts) => {
+      resProducts.body.forEach(function(element) {
+        fetch(process.env.FETCH_PRODUCTS_API, {
+            method: 'post',
+            body:    JSON.stringify(element),
+            headers: { 'Content-Type': 'application/json' },
+        }).then((res) => {
+            if(res.status != 200) {
+                console.log("unable to save products");
+            } else {
+                // console.log("saved products to DB");
+            }
+        });
+     });
+});
  
 
 
